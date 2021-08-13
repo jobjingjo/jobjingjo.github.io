91e0a6da2ed4390faa5d98a3ea1dcf5fb13c0d4f
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2021-08-13 08:48:16 +0700

    add offline

--- sw.js
+++ sw.js
@@ -8,4 +8,24 @@ if ('serviceWorker' in navigator) {
         console.log('ServiceWorker registration failed: ', err);
       });
     });
-  }
+}
+
+const CACHE_NAME = "offline";
+self.addEventListener('install', function(event) {
+
+  event.waitUntil(
+    (async () => {
+      await caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/images/icons-192.png',
+          '/images/icons-512.png',
+          '/sw.js'
+        ]);
+      })
+    })()
+  );
+  // Force the waiting service worker to become the active service worker.
+  self.skipWaiting();
+ });
