aa9e6bc02ed1a890719eb0db35cc7e23e4ef8362
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2021-08-13 09:00:41 +0700

    Load offline

--- sw.js
+++ sw.js
@@ -11,6 +11,7 @@ if ('serviceWorker' in navigator) {
 }
 
 const CACHE_NAME = "offline";
+const OFFLINE_URL = "offline.html";
 self.addEventListener('install', function(event) {
 
   event.waitUntil(
@@ -19,7 +20,7 @@ self.addEventListener('install', function(event) {
         return cache.addAll([
           '/',
           '/index.html',
-          '/offline.html',
+          OFFLINE_URL,
           '/images/icons-192.png',
           '/images/icons-512.png',
           '/sw.js'
@@ -30,3 +31,41 @@ self.addEventListener('install', function(event) {
   // Force the waiting service worker to become the active service worker.
   self.skipWaiting();
  });
+
+ self.addEventListener("fetch", (event) => {
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          // First, try to use the navigation preload response if it's supported.
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          // Always try the network first.
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          // catch is only triggered if an exception is thrown, which is likely
+          // due to a network error.
+          // If fetch() returns a valid HTTP response with a response code in
+          // the 4xx or 5xx range, the catch() will NOT be called.
+          console.log("Fetch failed; returning offline page instead.", error);
+
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
+  }
+
+  // If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
+});
